a2da2377ae7ebc4452c7c2967653a873612ba14b
wojtekO <wojore23@gmail.com>
2019-04-01 18:52:52 +0200

    start of project

---
+++ src/main/java/com/internet/inchirurgiademo/InchirurgiademoApplication.java
@@ -0,0 +1,13 @@
+package com.internet.inchirurgiademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InchirurgiademoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InchirurgiademoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/internet/inchirurgiademo/InchirurgiademoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.internet.inchirurgiademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InchirurgiademoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
